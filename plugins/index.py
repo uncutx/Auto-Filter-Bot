d7a96d9555b5a23d7c0c0feea92359d6ef6016a9
DesiXCloud <164381773+DesiXCloud@users.noreply.github.com>
2024-09-13 11:52:15 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -3,7 +3,7 @@ import time
 import asyncio
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait
-from info import ADMINS, INDEX_EXTENSIONS
+from info import ADMINS
 from database.ia_filterdb import save_file
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import temp, get_readable_time
